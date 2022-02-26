fb1ee8f8883d814efd9014aa4ac14d56aa5ea6f0
jehdaileg <jehdailegrand12@gmail.com>
2022-02-26 18:52:05 +0200

    Set up Laravel Websocket BEYOND CODE for installation and Basic Usage, set up queue configurations and pusher app keysm api and Configure the app.js file for Echp with Import and i;port meta for access to .env file to pick up the viteAppKey whcich was changed in .env fil from mix to vite and in package scripts

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,20 @@ import 'tailwindcss/tailwind.css';
 import AppLayout from "./layouts/AppLayout.vue";
 
 
+import Echo from "laravel-echo"
+
+import Pusher from "pusher-js";
+
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+    forceTLS: false,
+    disableStats: true,
+});
 
 const app = document.getElementById("app");
 
